ea18ebcdca0ae4eecbfb41b9c43879ead27b60f1
Zerrelius <Jeremy.Held@web.de>
2024-10-10 19:14:17 +0200

    Hotfix

--- shoppinglist.py
+++ shoppinglist.py
@@ -252,9 +252,10 @@ def main():
 
 if __name__ == "__main__":
     main()
+    # dickus = "Hello World!"
     # root = Tk()
     # frm = ttk.Frame(root, padding = 10)
     # frm.grid()
-    # ttk.Label(frm, text = "Hello World!").grid(column=0, row=0)
+    # ttk.Label(frm, text = dickus).grid(column=0, row=0)
     # ttk.Button(frm, text="Quit", command=root.destroy).grid(column=0, row=1)
     # root.mainloop()
